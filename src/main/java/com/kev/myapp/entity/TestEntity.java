989cb04a3857d670943448dd9db6f4ba46964ba5
KevinAGC <kevin2001garciacarrasco@gmail.com>
2023-02-08 19:37:37 -0600

    Nombre del Proyecto: my-app. Descripcion: Version final del proyecto my-app.

---
+++ src/main/java/com/kev/myapp/entity/TestEntity.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.kev.myapp.entity;
+
+/**
+ * @author kevin
+ *
+ */
+public class TestEntity {
+	/**
+	 * Propiedad de prueba para la muestra de compilacion del archivo testentity
+	 */
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	private String nombre;
+
+	
+}
